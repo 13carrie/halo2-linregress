f0fdfa43f8c493ff83a9f5cd53823517cbda4082
Caroline Smith <caroline.2.smith@kcl.ac.uk>
2024-12-12 15:06:36 +0800

    implement slope + intercept calculations

--- examples/lib_linregress.rs
+++ examples/lib_linregress.rs
@@ -4,7 +4,7 @@ use halo2_base::gates::RangeChip;
 use halo2_graph::gadget::fixed_point::{FixedPointChip, FixedPointInstructions};
 use halo2_base::utils::BigPrimeField;
 use halo2_base::AssignedValue;
-use halo2_base::{QuantumCell};
+use halo2_base::{QuantumCell, QuantumCell::Constant};
 
 
 #[allow(unused_imports)]
@@ -38,9 +38,7 @@ fn linear_regression_circuit<F: BigPrimeField>(
 
     // Convert field elements to QuantumCell<F>
     let x_quantum_cells: Vec<_> = x_values.iter().map(|&val| QuantumCell::Witness(val)).collect();
-    let x_quantum_cells_2 = x_quantum_cells.clone();
     let y_quantum_cells: Vec<_> = y_values.iter().map(|&val| QuantumCell::Witness(val)).collect();
-    let y_quantum_cells_2 = y_quantum_cells.clone();
 
 
     let a_decimal = input.a;
@@ -51,8 +49,6 @@ fn linear_regression_circuit<F: BigPrimeField>(
 
 
     // 2. compute sums (x, y, xy, x^2)
-    let n = x_values_decimal.len();
-    println!("n: {:?}", n);
 
     let sum_x: AssignedValue<F> = fixed_point_chip.qsum(ctx, x_values.iter().map(|&val| QuantumCell::Witness(val)));
     let sum_y: AssignedValue<F> = fixed_point_chip.qsum(ctx, y_values.iter().map(|&val| QuantumCell::Witness(val)));
@@ -78,12 +74,29 @@ fn linear_regression_circuit<F: BigPrimeField>(
 
 
     // 3. calculate slope and intercept using zkfixedpointchip
-    
+    let two = Constant(F::from(2));
+
+    let sum_y_sum_x2 = fixed_point_chip.qmul(ctx, sum_y, sum_x2);
+    let sum_x_sum_xy = fixed_point_chip.qmul(ctx, sum_x, sum_xy);
+    let sqrd_sum_x = fixed_point_chip.qpow(ctx, sum_x, two);
+
+    let intercept_numerator = fixed_point_chip.qsub(ctx, sum_y_sum_x2, sum_x_sum_xy);
+    let intercept_denominator = fixed_point_chip.qsub(ctx, sum_x2, sqrd_sum_x);
+    let intercept = fixed_point_chip.qdiv(ctx, intercept_numerator, intercept_denominator);
+
+
+    let n = Constant(F::from(x_values_decimal.len() as u64));
+    let n_sum_xy = fixed_point_chip.qmul(ctx, n, sum_xy);
+    let sum_x_sum_y = fixed_point_chip.qmul(ctx, sum_x, sum_y);
+    let n_sum_x2 = fixed_point_chip.qmul(ctx, n, sum_x2);
+    let slope_numerator = fixed_point_chip.qmul(ctx, n_sum_xy, sum_x_sum_y);
+    let slope_denominator = fixed_point_chip.qmul(ctx, n_sum_x2, sqrd_sum_x);
+    let slope = fixed_point_chip.qdiv(ctx, slope_numerator, slope_denominator);
 
 
     // 4. compare a and b with slope and intercept
-    // gate.assert_equal(ctx, slope_assigned, b_assigned);
-    // gate.assert_equal(ctx, intercept_assigned, a_assigned);
+    // assert_eq!(slope, b);
+    // assert_eq!(intercept, a);
 }
 
 fn main() {
